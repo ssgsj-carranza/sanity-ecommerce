82bf8f4c2be0410cea6fc589dc5e504da097b9e6
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-05-02 12:56:12 -0500

    working on pulling in products to main page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 import { Typography } from '@mui/material'
 import Head from 'next/head'
+import { useEffect, useState } from 'react';
 import Layout from '../components/Layout'
+import client from '../utils/client';
 
 export default function Home() {
+  const [state, setState] = useState({
+    products: [],
+    error: '',
+    loading: true,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const products = await client.fetch(`*[_type == "product"]`);
+        setState({ products, loading: false });
+      } catch (err) {
+        setState({ loading: false, error: err.message });
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       List Products
@@ -17,3 +37,5 @@ export default function Home() {
 // npx tailwindcss init -p
 // npm install @emotion/styled
 // npm install @emotion/server @emotion/react
+// npm install -g @sanity/cli
+// npm install @sanity/client
---
+++ utils/config.js
@@ -0,0 +1,5 @@
+const config = {
+    projectId: 'pyi722y2i',
+    dataset: 'production',
+  };
+  export default config;
